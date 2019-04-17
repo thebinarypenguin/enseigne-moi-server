aac7499e2416d14d47527e695704411c48703b5d
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-17 12:36:20 -0400

    Rename list to LinkedList,implement it

---
+++ src/language/LinkedList.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(val, next) {
+    this.val = val || null;
+    this.next = next || null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Find Node with value val and move it down (away from head) one position
+  moveDown(id) {
+
+    let previousNode = null;
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (currentNode.val.id === id) {
+        let nextNodeCopy = currentNode.next;
+
+        // currentNode is the first item
+        if (previousNode === null) {
+          currentNode.next = currentNode.next.next;
+          nextNodeCopy.next = currentNode;
+          this.head = nextNodeCopy;
+
+          return;
+        }
+
+        // currentNode is the last item
+        if (currentNode.next === null) {
+          return;
+        }
+
+        // currentNode is somewhere in the middle
+        currentNode.next = currentNode.next.next;
+        nextNodeCopy.next = currentNode;
+        previousNode.next = nextNodeCopy;
+
+        return;
+      }
+
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+    }
+  }
+
+  insertFirst(item) {
+
+    if (!this.head) {
+      this.head = new Node(item, null);
+    } else {
+      this.head = new Node(item, this.head);
+    }
+  }
+}
+
+module.exports = LinkedList;
